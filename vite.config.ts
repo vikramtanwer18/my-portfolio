cf0842f68c63d63456c0a08ea51521de62541270
vikramFonix <vikram.saini@Fonixtech.io>
2025-09-04 15:38:31 +0530

    index file update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';  // Use official React plugin
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),  // Use the official plugin
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
